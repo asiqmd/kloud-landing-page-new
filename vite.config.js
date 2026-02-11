38a9e98b97080a2d72919ce5a35bfeaab3b61c3a
Asiq Mohammed <asiqmohammd@gmail.com>
2026-02-11 17:49:57 +0600

    Added package-lock.json

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        '404': '404.html',
+      },
+    },
+  },
+});
